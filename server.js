c19d9a9548611c9b263f318f7c8ca1215a99c6a4
GeekInterrupted <GeekInterrupted@users.noreply.github.com>
2017-07-14 21:47:54 -0500

    made changes

--- server.js
+++ server.js
@@ -3,44 +3,7 @@ var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 
 
-//mongoose schema definition
 
-Schema = new mongoose.Schema({
-    id: String,
-    name: String,
-    class: String,
-    legs: Number,
-    weight: Number,
-    petName: String,
-    reference: String
-});
-
-Animal = mongoose.model("Animal", Schema);
-
-
-// Here we find an appropriate database to connect to, defaulting to
-// localhost if we don't find one.  
-
-var uristring =
-    process.env.MONGODB_URI || 'mongodb://localhost/bondanimals';
-
-// The http server will listen to an appropriate port, or default to
-// port 5000.
-
-var theport = process.env.PORT || 5000;
-
-// Makes connection asynchronously.  Mongoose will queue up database
-// operations and release them when the connection is complete.
-
-mongoose.connect(uristring, function(err, res) {
-    if (err) {
-        console.log('ERROR connecting to: ' + uristring + '. ' + err);
-    } else {
-        console.log('Succeeded connected to: ' + uristring);
-    }
-
-});
-var db = mongoose.connection;
 
 var app = express();
 app.use(bodyParser.json()) // support json encoded bodies
@@ -49,5 +12,5 @@ app.use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res) {
-    res.send("Hello world");
+    res.render("index");
 });
