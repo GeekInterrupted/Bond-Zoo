d34c25484076f7fcd6fe3f6a2323c029b7f663bb
GeekInterrupted <GeekInterrupted@users.noreply.github.com>
2017-07-14 22:02:34 -0500

    made changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var app = express();
 app.use(bodyParser.json()) // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/"));
 
 app.get("/", function(req, res) {
     res.send(index.html);
