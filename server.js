c470ff6f2cd8dc2c01fb7e87ae6a2ce15b02d7cc
GeekInterrupted <GeekInterrupted@users.noreply.github.com>
2017-07-14 20:59:15 -0500

    edited files

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res) {
-    res.send("hello world!");
+    res.render("index");
 });
 
 app.get("/all", function(req, res) {
